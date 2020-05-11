2d6ffd5e977b4ac75e89a5fde7e84512197827b4
root <innocdavid@gmail.com>
2020-05-11 11:44:05 -0400

    another commit

---
+++ listComprehension1.py
@@ -0,0 +1,27 @@
+#TODO:
+"""
+Writing a program that reads a file and
+stips off the leading and trailing whitespaces
+in the file and store everythong in a list of
+strings
+"""
+
+#Code to read the file
+filename = 'listComprehension0.py'
+
+f = open(filename)
+lines = []
+
+for i in f:
+    lines.append(i.strip())
+
+print(lines)
+
+
+#TODO:
+"""
+Putting in a single line of code
+"""
+
+print([i.strip() for i in open('listComprehension1.py') ])
+
